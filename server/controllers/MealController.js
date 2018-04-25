01c9ded0c029630911b13c352382c4abda444a1b
Mbonu Blessing <mbonunkemjika@ymail.com>
2018-04-25 08:01:57 +0100

    Completed the modify meal API
    - Added a function to modify a meal by the id of the meal and the content of the request in the database
    - Updated the router file
    
    [Finishes #156932446]

--- server/controllers/MealController.js
+++ server/controllers/MealController.js
@@ -14,7 +14,7 @@ const MealController = {
       req.body.isChecked,
     );
     const filter = meal.data.filter(checkName => checkName.name === req.body.name);
-    if (filter === []) {
+    if (filter.length === 0) {
       meal.data.push(newMeal);
       const mealDataUpdate = JSON.stringify(meal, null, 2);
       return fs.writeFile(path.join(`${__dirname}/../database/mealDatabase.json`), mealDataUpdate, (err) => {
@@ -27,13 +27,33 @@ const MealController = {
     return res.status(200).send({ message: 'Meal already exist' });
   },
   getAllMeal(req, res) {
-    fs.readFile(path.join(`${__dirname}/../database/mealDatabase.json`), (err, data) => {
+    fs.readFile((path.join(`${__dirname}/../database/mealDatabase.json`)), 'utf8', (err, data) => {
       const meal = JSON.parse(data);
       if (err) {
         return res.status(400).send({ message: 'Database not found' });
       }
       return res.status(200).send(meal);
     });
+  },
+  modifyOneMeal(req, res) {
+    const data = fs.readFileSync(path.join(`${__dirname}/../database/mealDatabase.json`));
+    const meal = JSON.parse(data);
+    const filtered = meal.data.filter(checkProperty => checkProperty.id === Number(req.params.id));
+    if (filtered.length === 1) {
+      const position = meal.data.indexOf(filtered[0]);
+      const checkKeys = Object.keys(req.body);
+      checkKeys.forEach((dataKey) => {
+        meal.data[position][dataKey] = req.body[dataKey];
+      });
+      const mealDataUpdate = JSON.stringify(meal, null, 2);
+      return fs.writeFile(path.join(`${__dirname}/../database/mealDatabase.json`), mealDataUpdate, (err) => {
+        if (err) {
+          return res.status(400).send({ message: 'Meal not added successfully' });
+        }
+        return res.status(200).send(meal.data[position]);
+      });
+    }
+    return res.status(200).send({ message: 'Meal does not exisit' });
   },
 };
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,9 @@
 import MealController from '../controllers/MealController';
 
 const routes = (app) => {
-  app.use((req, res, next) => {
-    console.log(req.body, req.method, req.url);
-    next();
-  });
-
-  app
-    .post('/api/v1/meals', MealController.addOneMeal)
-    .get('/api/v1/meals', MealController.getAllMeal);
+  app.post('/api/v1/meals', MealController.addOneMeal);
+  app.get('/api/v1/meals', MealController.getAllMeal);
+  app.put('/api/v1/meals/:id', MealController.modifyOneMeal);
 };
 
 export default routes;
