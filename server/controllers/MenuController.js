c68b20e7fc9afbe918b47de42ab9d8aef5dcd705
Mbonu Blessing <mbonunkemjika@ymail.com>
2018-04-26 07:12:26 +0100

    Modify orders API
    - Created the API to modify an order made
    - Updated the routes folder
    - Updated the README to add code climate and coverall badges
    - Updated the menu controller file
    
    [Finishes #156910940]

--- server/controllers/MenuController.js
+++ server/controllers/MenuController.js
@@ -2,43 +2,42 @@ import fs from 'fs';
 import path from 'path';
 import Menu from '../models/menu';
 
-
 const MenuController = {
   createMenu(req, res) {
     const mealData = fs.readFileSync(path.join(`${__dirname}/../database/mealDatabase.json`));
     const meal = JSON.parse(mealData);
     const menuData = fs.readFileSync(path.join(`${__dirname}/../database/menuDatabase.json`));
-    const menu = JSON.parse(menuData);
+    const menuParsed = JSON.parse(menuData);
     const filter = meal.data.filter(checkName => checkName.isChecked === 'true');
-    if (!menu[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`]) {
-      menu[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`] = new Menu(req.body.id, filter);
-      const menuDataUpdate = JSON.stringify(menu, null, 2);
+    if (!menuParsed[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`]) {
+      menuParsed[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`] = new Menu(req.body.id, filter);
+      const menuDataUpdate = JSON.stringify(menuParsed, null, 2);
       return fs.writeFile(path.join(`${__dirname}/../database/menuDatabase.json`), menuDataUpdate, (err) => {
         if (err) {
           return res.status(400).send({ message: 'Meal not added successfully' });
         }
-        return res.status(200).send(menu);
+        return res.status(200).send(menuParsed);
       });
     }
-    if (menu[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`]) {
-      menu[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`].menu = filter;
-      const menuDataUpdate = JSON.stringify(menu, null, 2);
+    if (menuParsed[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`]) {
+      menuParsed[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`].menu = filter;
+      const menuDataUpdate = JSON.stringify(menuParsed, null, 2);
       return fs.writeFile(path.join(`${__dirname}/../database/menuDatabase.json`), menuDataUpdate, (err) => {
         if (err) {
           return res.status(400).send({ message: 'Meal not added successfully' });
         }
-        return res.status(200).send(menu);
+        return res.status(200).send(menuParsed);
       });
     }
     return res.status(200).send({ message: 'Menu created successfully' });
   },
   getMenu(req, res) {
     fs.readFile((path.join(`${__dirname}/../database/menuDatabase.json`)), 'utf8', (err, data) => {
-      const menu = JSON.parse(data);
+      const menuParsed = JSON.parse(data);
       if (err) {
         return res.status(400).send({ message: 'Database not found' });
       }
-      return res.status(200).send(menu[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`].menu);
+      return res.status(200).send(menuParsed[`${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`].menu);
     });
   },
 };
